c8135b2cb5885f03cc70ca2aaa555bd0923a6575
Minh <nhungocminh2004@gmail.com>
2025-03-15 00:23:14 +0700

    middlewares
    d

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -2,27 +2,50 @@ import express from 'express';
 import connectDB from '../config/connectDB.js';
 import productModel from '../models/productModel.js'
 // api add product
-const addProduct =async (req,res)=>{
-    try {
+const addProduct = async (req, res) => {
+    try { 
         connectDB();
-        const {name,category,brand,stock_quantity}=req.body
-        if (!name ||!category ||!brand ||!stock_quantity){
-            return res.json({success:false,message:"Missing Details"}) // missing sth
+        const { name, price, description, category, stock_quantity,brand } = req.body;
+        const imageFile = req.file;
+
+        // Kiểm tra xem đủ thông tin chưa
+        if (!name || !price || !category || !stock_quantity ||!brand ) {
+            return res.json({ success: false, message: "Missing product details" });
         }
-        const productData={
+
+        // Kiểm tra giá và số lượng phải là số dương
+        if (price <= 0) {
+            return res.json({ success: false, message: "Price must be positive and stock cannot be negative" });
+        }
+
+        // Upload ảnh sản phẩm lên Cloudinary (nếu có)
+        let imageURL = "";
+        if (imageFile) {
+            const imageUpload = await cloudinary.uploader.upload(imageFile.path, { resource_type: "image" });
+            imageURL = imageUpload.secure_url;
+        }
+
+        const productData = {
             name,
+            price,
+            description,
             category,
             brand,
-            stock_quantity
-        }
-        const newProduct= new productModel(productData)
-        const pr= newProduct.save();
-        return res.json({success:true,data:pr}); 
+            stock_quantity,
+            image: imageURL,
+            dateAdded: Date.now()
+        };
+
+        const newProduct = new productModel(productData);
+        await newProduct.save();
+
+        res.json({ success: true, message: "Product added successfully" });
     } catch (error) {
-        console.log(error.message);
-        
+        console.log(error);
+        res.json({ success: false, message: error.message });
     }
-}
+};
+
 export {
     addProduct
 }
--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,17 @@ import cors from 'cors';
 import 'dotenv/config';
 import adminRouter from './routes/adminRouter.js';
 import userRouter from './routes/userRouter.js';
-import connectDB from './config/connectDB.js';
 import bodyParser from 'body-parser';
+import connectDB from './config/connectDB.js';
 const app = express();
 app.use(express.json());
 const port=process.env.PORT || 4000;
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }))
+connectDB();
 //app.use('/api/admin',adminRouter);
 app.use('/api/user',userRouter);
 app.use('/api/admin',adminRouter);
-app.get('/',(req, res) => { connectDB(); res.send("api working")});
+app.get('/',(req, res) => { res.send("api working")});
 app.listen(port,()=>console.log("Server started",port));
 
